806ae3b1f6e09f1ba3d83894eb42917d439c16b0
Shahzaib <shahzaibux@gmail.com>
2020-04-02 19:20:58 +0500

    Create Front End

---
+++ OilGasCompany.Web/bundles/scripts/month.js
@@ -0,0 +1,8 @@
+if (!window.XOM) {
+    window.XOM = {};
+  }
+  window.XOM.monthsShort = ["Jan.","Feb.","March","April","May","June","July","Aug.","Sept.","Oct.","Nov.","Dec."];
+  
+  if (!window.XOM.CHARTS) {
+    window.XOM.CHARTS = {};
+  }
