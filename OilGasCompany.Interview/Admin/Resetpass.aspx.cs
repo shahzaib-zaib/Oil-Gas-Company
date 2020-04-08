66d49400488ffd7eac80ccc5516bd7fb6630d3e1
Shahzaib <shahzaibux@gmail.com>
2020-04-08 12:53:06 +0500

    Updated Resetpass in Panel

--- OilGasCompany.Interview/Admin/Resetpass.aspx.cs
+++ OilGasCompany.Interview/Admin/Resetpass.aspx.cs
@@ -16,11 +16,11 @@ namespace OilGasCompany.Interview.Admin
 
         }
 
-        protected void btn_resetpass_Click(object sender, EventArgs e)
+        protected void btn_ResetPass_Click(object sender, EventArgs e)
         {
             if (IsValid)
             {
-                string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+                string cs = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
                 using (SqlConnection con = new SqlConnection(cs))
                 {
                     SqlCommand cmd = new SqlCommand("update admin set admin_password= @adminpass where admin_password = @adminoldpassword", con);
@@ -32,28 +32,28 @@ namespace OilGasCompany.Interview.Admin
                         int i = (int)cmd.ExecuteNonQuery();
                         if (i > 0)
                         {
-                            Response.Redirect("~/admin/index.aspx");
+                            Response.Redirect("~/Admin/Index.aspx");
                         }
                         else
                         {
                             txt_admipresentpass.Focus();
-                            panel_resetpass_warning.Visible = true;
-                            lbl_resetpasswarning.Text = "Something went wrong. Can't update. Please try after sometime later</br> ";
+                            panel_ResetPass_Warning.Visible = true;
+                            lbl_ResetPassWarning.Text = "Something went wrong. Can't update. Please try after sometime later</br> ";
                         }
                     }
                     catch (Exception ex)
                     {
                         txt_admipresentpass.Focus();
-                        panel_resetpass_warning.Visible = true;
-                        lbl_resetpasswarning.Text = "Something went wrong. Please try after sometime later</br> Contact you developer for this problem" + ex.Message;
+                        panel_ResetPass_Warning.Visible = true;
+                        lbl_ResetPassWarning.Text = "Something went wrong. Please try after sometime later</br> Contact your developer for this problem" + ex.Message;
                     }
                 } // end of using 
             }
             else
             {
                 txt_admipresentpass.Focus();
-                panel_resetpass_warning.Visible = true;
-                lbl_resetpasswarning.Text = "You must fill all the requirements";
+                panel_ResetPass_Warning.Visible = true;
+                lbl_ResetPassWarning.Text = "You must fill all the requirements";
             }
         }
     }
